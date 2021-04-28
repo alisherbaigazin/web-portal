acd4cee78c707307b11b6e50de15a1801edba4da
beka <b.zhanaikhanov@mycar.kz>
2021-04-29 01:57:56 +0600

    project

--- web_portal/cms_plugins.py
+++ web_portal/cms_plugins.py
@@ -2,7 +2,8 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import gettext_lazy as _
 
-from .models import Hello
+from .models import Hello, Speciality, SpecialityList
+
 
 @plugin_pool.register_plugin
 class HelloPlugin(CMSPluginBase):
@@ -14,3 +15,28 @@ class HelloPlugin(CMSPluginBase):
     def render(self, context, instance, placeholder):
         context = super().render(context, instance, placeholder)
         return context
+
+
+@plugin_pool.register_plugin
+class SpecialityItemPlugin(CMSPluginBase):
+    model = Speciality
+    name = _("Speciality Item Plugin")
+    render_template = "speciality_item_plugin.html"
+    cache = False
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
+
+
+@plugin_pool.register_plugin
+class SpecialityListPlugin(CMSPluginBase):
+    model = SpecialityList
+    name = _("Speciality List Plugin")
+    render_template = "speciality_list_plugin.html"
+    cache = False
+
+    def render(self, context, instance, placeholder):
+        context = super().render(context, instance, placeholder)
+        return context
+
--- web_portal/models.py
+++ web_portal/models.py
@@ -1,6 +1,44 @@
 from cms.models.pluginmodel import CMSPlugin
 
 from django.db import models
+from cms.models.pagemodel import Page
+
 
 class Hello(CMSPlugin):
     guest_name = models.CharField(max_length=50, default='Guest')
+
+
+class Speciality(CMSPlugin):
+    name = models.CharField(max_length=50, default='name')
+    short_description = models.CharField(max_length=100, default='short_description')
+    description = models.CharField(max_length=200, default='description')
+    footer = models.CharField(max_length=50, default='footer')
+    image = models.ImageField()
+    page_link = models.ForeignKey(Page, on_delete=models.CASCADE, related_name='page_link', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SpecialityList(CMSPlugin):
+    list_title = models.CharField(max_length=50, default='list_title')
+    short_description = models.CharField(max_length=100, default='short_description')
+    description = models.CharField(max_length=200, default='description')
+    related_link = models.ManyToManyField(blank=True, to='Speciality', verbose_name="Related List")
+
+    def paired_list(self):
+        paired_spec_list = []
+        specs = self.related_link.all()
+        sz = len(specs)
+        print(sz)
+
+        for i in range(0, sz, 2):
+            if i+1 >= sz or sz < 2:
+                paired_spec_list.append((specs[i], None))
+            else:
+                paired_spec_list.append((specs[i], specs[i+1]))
+
+        return paired_spec_list
+
+    def copy_relations(self, oldinstance):
+        self.related_link.set(oldinstance.related_link.all())
